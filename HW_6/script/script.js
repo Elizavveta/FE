f01baecb62e9f3a205c467c710ffc440b810f96a
Elizavveta <137884565+Elizavveta@users.noreply.github.com>
2023-08-30 22:20:22 +0300

    HW_6

---
+++ HW_6/script/script.js
@@ -0,0 +1,38 @@
+// for(let i = 0; i <= 10; i +=2){
+//     console.log(i);
+// }
+
+
+
+// for(let i = 55; i > 21; i--){
+//     console.log(i);
+// }
+
+
+
+// const numbers = [3, 5, 11, 2, 8, 1, 6]
+// const numbers_squared = []
+
+// for(let i = 0; i < numbers.length; i++){
+//     console.log(numbers[i]);
+// }
+
+// for(let i = 0; i < numbers.length; i++){
+//     const squared = numbers[i] ** 2;
+//     numbers_squared.push(squared)
+// }
+// console.log(numbers_squared);
+
+
+
+// const last_elem = numbers_squared[numbers_squared.length - 1]
+// console.log(last_elem);
+
+
+const user = {
+    first_name: 'Ivan', 
+    last_name: 'Ivanov', 
+    age: 20, 
+    salary: 500
+    }
+    console.log(`user's name is ${user.first_name} ${user.last_name}. He is ${user.age} years old.`)
