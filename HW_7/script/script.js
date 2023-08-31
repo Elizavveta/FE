38d31dd5f7f40f3c7ddd58d37def1b6370b7a4fb
Elizavveta <137884565+Elizavveta@users.noreply.github.com>
2023-09-01 00:29:25 +0300

    HW_7

---
+++ HW_7/script/script.js
@@ -0,0 +1,63 @@
+// 1. function minNum(a, b) {
+//     if (a<b) {
+//         console.log(a);
+//     }else{
+//         console.log(b);
+//     } 
+// }
+// minNum(7, 9)
+
+//2.
+//  function numbers(num1, num2) {
+//     let maxNum = Math.max (num1, num2)
+//     let minNum = Math.min (num1, num2)
+
+//     for( let i = maxNum; i>= minNum; i--){
+//         if (i % 2 === 0) {
+//             console.log(i);
+//         }
+//     }
+// }
+// numbers(20, 7)
+
+
+//3.
+// function power(num1, num2 = 2) {
+//     console.log(num1 ** num2);
+    
+// }
+// power(7)
+
+//4.
+// function sumNum(n) {
+//     let sum = 0
+//     for(let i =1; i<=n; i++){
+//         sum += i
+//     }
+//     return sum
+// }
+// let n = 7
+// let result = sumNum(n)
+// console.log(`сумма чисел от 1 до ${n} равняется ${result}`);
+
+//5.
+// function sumNum(n, m) {
+// let sumEven = 0
+// let sumOdd = 0
+
+// for (let i = n; i <= m; i++){
+//     if(i % 2 === 0){
+//         sumEven += i
+//     }else{
+//         sumOdd +=i
+//     }
+// }
+// console.log(`sum of Even numbers from ${n} till ${m}: ${sumEven} `);
+// console.log(`sum of Odd numbers from ${n} till ${m}: ${sumOdd} `);
+// }
+// let n = 2
+// let m = 16
+// sumNum(n,m)
+
+//6.
+//не смогла понять как это сделать((
